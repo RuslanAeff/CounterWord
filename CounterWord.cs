5a328b6db4f00df29dcbcfa2987fcbf7ddd071f6
Ruslan Aliyev <ruslanra@code.edu.az>
2023-07-10 16:15:48 +0400

    first commit

---
+++ CounterWord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Countword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "CodeAcademy is a good place to study";
+            string[] result = sentence.Split(' ');
+            Console.WriteLine($"Total number of words in the string is:{result.Length}");
+        }
+    }
+}
